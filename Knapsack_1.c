23a9e00aabdd495b4551ccb6f230cd5c1099239a
brainnoopen <793536301@qq.com>
2018-10-05 14:22:52 +1000

    want to read data from test

--- Knapsack_1.c
+++ Knapsack_1.c
@@ -9,16 +9,31 @@ int* values; //Array containing value of all items
 int W; //Capacity of KnapSack
 int** F; //Memory Function
 int data; //record all the data from input
-int main()
+int main(int argc, char** argv)
 {
-	FILE *in_file  = fopen(argv[1], "r");
-    if (in_file  == NULL)
+
+	FILE *fp  = fopen(argv[1], "r");
+    if (fp  == NULL)
     {
         printf("Can't open file for reading.\n");
         return -1;
     }
-    fscanf(in_file,"%d", &data);
-    printf("%d", data);
+    
+    char line[256];
+    while (fgets(line, sizeof(line), fp)) {
+        /* note that fgets don't strip the terminating \n, checking its
+           presence would allow to handle lines longer that sizeof(line) */
+        printf("%s", line); 
+    }
+    for(int t=1; t<250;t++){
+    	printf("%s", line[t]);
+    }
+    
+    /* may check feof here to make a difference between eof and io failure -- network
+       timeout for instance */
+
+    fclose(fp);
+
 	scanf("%d", &n);
 	weights = (int*)malloc((n+1)*sizeof(int));
 	values = (int*)malloc((n+1)*sizeof(int));
